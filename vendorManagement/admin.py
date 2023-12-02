52e907c3378b8e54f6c946480abb69ac3e4bfc7d
ShaikRiyaz786r <reehuriyaz786@gmail.com>
2023-12-02 16:02:21 +0530

    final commit

--- vendorManagement/admin.py
+++ vendorManagement/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
+from .models import *
 
 
+class VendorAdmin(admin.ModelAdmin):
+    list_display = ['name','contact','details','address','vendor_code']
 
+admin.site.register(Vendor,VendorAdmin)
 
 
 
